57a8404129958966a62305dc175688604fc19892
gabroberge <gabrielroberge01@gmail.com>
2023-06-18 18:44:29 -0400

    fix decorator metadata not being emitted

---
+++ .changeset/brave-laws-remember.md
@@ -0,0 +1,5 @@
+---
+'@gabroberge/nestjs-dataloader': minor
+---
+
+fix decorator metadata not being emitted
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import path from 'path';
+import swc from 'unplugin-swc';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
+	plugins: [swc.rollup(), swc.vite()],
 	resolve: {
 		alias: {
 			'~': path.resolve(__dirname, 'src'),
